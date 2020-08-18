43fd061fe6802962e63d44595ae0023293c63368
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-08-18 19:51:12 +0300

    charts style

--- src/Components/CustomCart.tsx
+++ src/Components/CustomCart.tsx
@@ -2,30 +2,46 @@ import * as React from 'react';
 import 'react-vis/dist/style.css';
 import { XYPlot, LineSeries, XAxis, YAxis } from 'react-vis';
 import { Paper } from '@material-ui/core';
+import styled from 'styled-components';
 
-const CustomCart: React.FunctionComponent = () => {
+const data = [
+    { x: 0, y: 8 },
+    { x: 1, y: 7 },
+    { x: 2, y: 6 },
+    { x: 3, y: 5 },
+    { x: 4, y: 4 },
+    { x: 5, y: 5 },
+    { x: 6, y: 6 },
+    { x: 7, y: 7 },
+    { x: 8, y: 6 },
+    { x: 9, y: 0 }
+];
 
-    const data = [
-        { x: 0, y: 8 },
-        { x: 1, y: 7 },
-        { x: 2, y: 6 },
-        { x: 3, y: 5 },
-        { x: 4, y: 4 },
-        { x: 5, y: 5 },
-        { x: 6, y: 6 },
-        { x: 7, y: 7 },
-        { x: 8, y: 6 },
-        { x: 9, y: 0 }
-    ];
+interface IProps {
+    big?: boolean;
+    title?: string;
+    total?: string;
+    subTitle?: string;
+    xy?: { x: number, y: number }[];
+    color?: string;
+}
+
+const StyledPaper: any = styled(Paper)`
+    padding: 0.5%;
+`;
+
+const CustomCart: React.FunctionComponent<IProps> = ({ big = false, color, total, title, xy, subTitle }) => {
 
     return (
-        <Paper>
-            <XYPlot height={160} width={235}>
-                <YAxis />
-                <XAxis />
-                <LineSeries curve={'curveMonotoneX'} data={data} />
+        <StyledPaper>
+            {title} <br />
+            {big ? null : `$${total || ''}`}
+            <XYPlot style={{ color: 'black' }} height={110} width={big ? 650 : 235}>
+                {big ? <YAxis /> : null}
+                {big ? <XAxis /> : null}
+                <LineSeries curve={'curveMonotoneX'} data={xy || data} />
             </XYPlot>
-        </Paper>
+        </StyledPaper>
     );
 }
 
--- src/Containers/HomePage.tsx
+++ src/Containers/HomePage.tsx
@@ -19,6 +19,7 @@ const StyledDiv = styled.section`
         flex-direction: column;
         justify-content: space-between;
         height: 100%;
+        margin-top: 1%;
     }
 `;
 
@@ -26,18 +27,44 @@ const HomePage: React.FunctionComponent = () => {
 
     const localData: Array<any> = useSelector((state: any) => state.app.data);
 
+    const demoXY = [
+        { x: 0, y: 0 },
+        { x: 1, y: 7 },
+        { x: 2, y: 6 },
+        { x: 3, y: 5 },
+        { x: 4, y: 4 },
+        { x: 5, y: 5 },
+        { x: 6, y: 6 },
+        { x: 7, y: 7 },
+        { x: 8, y: 6 },
+        { x: 9, y: 0 }
+    ];
+
     return (
         <StyledDiv>
             Dashboard
             <div className='MainDiv'>
                 <div className='ChartsDiv'>
-                    <CustomCart />
-                    <CustomCart />
-                    <CustomCart />
+                    <CustomCart
+                        xy={demoXY}
+                        title='Sales'
+                        total='5023'
+                    />
+                    <CustomCart
+                        title='Earn Money'
+                        total='6326'
+                    />
+                    <CustomCart
+                        title='Order'
+                        total='4249'
+                    />
                 </div>
                 <div>Will be Cards</div>
                 <div>
-                    <CustomCart />
+                    <CustomCart
+                        big={true}
+                        title='Revenue'
+                    />
                 </div>
             </div>
         </StyledDiv>
